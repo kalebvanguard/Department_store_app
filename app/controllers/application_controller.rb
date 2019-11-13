033ead96b7aea394f8c14b3323236badb36e6587
Josh <joshuakearl@gmail.com>
2019-11-13 16:23:17 -0700

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+
+    # class ApplicationController < ActionController::Base
+    #     before_action :verify_paid
+      
+    #     def verify_paid
+    #       @paid = true
+    #     end
+    #   end
+
+
 end
--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,16 +1,65 @@
 class DepartmentsController < ApplicationController
+  before_action :set_sub, only: [:show, :update, :edit, :destroy]
+
   def index
-  end
+    @departments = Department.all
+      end
 
   def show
+    @departments = Department.all
   end
 
   def new
+    @department = departments.new
+    render partial: "form"
   end
 
   def edit
+    #GET 
+    render partial: "form"
+  
   end
 
-  def skip-routes
+
+
+  def create
+    @department = Department.new(department_params)
+    
+    if @departmnet.save
+      redirect_to departments_path
+    else
+      render :new
+    end
   end
+
+  
+ 
+  private
+ 
+    def pages_params
+      params.require(:page).permit(:title, :author, :body)
+    end
+
+  def update
+    if @department.update(department_params)
+      redirect_to @department
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @department.destroy
+    redirect_to departments_path
+  end
+
+  private
+    
+    def set_sub
+      @department = Department.find(params[:id])
+    end
+
+    def sub_params
+      params.require(:department).permit(:name)
+    end
 end
